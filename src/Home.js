a0443de5ce2a813c23a0bd01143c93108658089a
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-03 19:41:39 -0500

    Added state to Home component using useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function Home() {
+	const [name, setName] = useState("Ayon");
+	const [age, setAge] = useState(25);
+
+	function handleClick() {
+		console.log("You clicked the button.");
+		setName(`${name === "Ayon" ? "Maliat" : "Ayon"}`);
+		setAge(`${age === 25 ? 18 : 25}`);
+	}
+
+	function greetUser(name) {
+		console.log(`Welcome to Dojo Blog, ${name}!`);
+	}
 	return (
 		<div className="home">
 			<h2>Home page</h2>
+			<p>
+				{name} is {age} years old.
+			</p>
+			<button onClick={handleClick}>Click me!</button>
+			<button onClick={() => greetUser("Mushfiq")}>Click me too!</button>
 		</div>
 	);
 }
