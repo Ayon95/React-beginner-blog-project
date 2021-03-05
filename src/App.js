c32bcf8a0aef3794f24b655a415851edcc1dcece
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-05 11:21:27 -0500

    Added a new component Create

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar.js";
 import Home from "./Home.js";
+import Create from "./Create.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -9,8 +10,12 @@ function App() {
 				<Navbar />
 				<div className="content">
 					<Switch>
-						<Route path="/">
+						<Route exact path="/">
 							<Home />
+						</Route>
+
+						<Route path="/create">
+							<Create />
 						</Route>
 					</Switch>
 				</div>
