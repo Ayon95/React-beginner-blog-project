a65ea0c16ccb4ed512c6dd8bf60021d808162c58
Ayon95 <mushfiqurrahman78@yahoo.com>
2021-03-04 19:31:43 -0500

    Set up a json server and wrote code for fetching blogs data from the server

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList.js";
 
 function Home() {
-	const [blogs, setBlogs] = useState([
-		{ title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-		{ title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-		{ title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
-	]);
-
-	const [name, setName] = useState("mario");
+	const [blogs, setBlogs] = useState(null);
 
 	function handleDelete(id) {
 		const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -16,16 +10,21 @@ function Home() {
 		setBlogs(newBlogs);
 	}
 
+	// using the effect hook to fetch data after the first render
+	// no need to worry about starting infinite loop due to changing state because we are using an empty dependency array
 	useEffect(() => {
-		console.log("use effect ran");
-		console.log(name);
-	}, [name]);
+		// fetching and loading data
+		fetch("http://localhost:8000/blogs")
+			.then((response) => response.json())
+			.then((data) => {
+				setBlogs(data);
+				console.log(data);
+			});
+	}, []);
 
+	// here we are conditionally templating BlogList component
 	return (
-		<div className="home">
-			<BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-			<button onClick={() => setName(`${name === "mario" ? "luigi" : "mario"}`)}>Change name</button>
-		</div>
+		<div className="home">{blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />}</div>
 	);
 }
 
